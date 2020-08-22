46e977226387dcadde6149c7c0089cec43db395a
Zayed <zayed.cs3@gmail.com>
2020-08-22 16:04:44 +0600

    Login User Added

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import user_signup
+from .views import user_signup, user_login
 
 
 urlpatterns = [
 
         path('signup/', user_signup, name='user-signup'),
+        path('login/', user_login, name='user-login'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
 from django.db import IntegrityError
 
 # Create your views here.
@@ -20,9 +21,24 @@ def user_signup(request):
             try:
                 user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                messages.success(request, "Account Created Successfully. ")
-                return redirect('home-view')
+                messages.success(request, "Account Created Successfully. You Can Now Login. ")
+                return redirect('user-login')
             except IntegrityError:
-                messages.error(request, "Username already Taken. Choose a new one. ")
+                messages.info(request, "Username already Taken. Choose a new one. ")
                 return render(request, 'users/signup.html', context)
     return render(request, 'users/signup.html', context)
+
+
+def user_login(request):
+    context = {
+        'form' : AuthenticationForm()
+    }
+    if request.method == "POST":
+        user = authenticate(request, username = request.POST['username'], password= request.POST['password'])
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home-view')
+        else:
+            messages.info(request, 'Wrong Credential. Try Again. ')
+            return render(request, 'users/login.html', context)
+    return render(request, 'users/login.html', context)
